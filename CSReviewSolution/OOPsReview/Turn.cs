e61b79287eb7b90a6964c33465f93db5af018e91
smeszaros2 <46729299+smeszaros2@users.noreply.github.com>
2019-01-21 08:18:50 -0700

    Add files via upload

---
+++ CSReviewSolution/OOPsReview/Turn.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsReview
+{
+    public class Turn
+    {
+        public int Player1 { get; set; }
+        public int Player2 { get; set; }
+        //optionally, for practice only add constructors
+
+        public Turn()
+        {
+
+        }
+
+        public Turn(int player1, int player2)
+        {
+            Player1 = player1;
+            Player2 = player2;
+        }
+
+    }
+}
